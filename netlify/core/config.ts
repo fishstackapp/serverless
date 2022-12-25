45ca5c7d0fb2917cd30e83663c0bc647f5e363df
Oleh Poliakov <106983325+OlehPoliakov@users.noreply.github.com>
2022-12-25 17:07:08 +0200

    fix

--- netlify/core/config.ts
+++ netlify/core/config.ts
@@ -3,11 +3,11 @@ export const config = {
   hasuraEndpoint: process.env.HASURA_ENDPOINT,
   hasuraAdminSecret: process.env.HASURA_ADMIN_SECRET,
   hasuraFishstackSecret: process.env.HASURA_FISHSTACK_SECRET,
+  // MISC
+  jwtSecret: process.env.JWT_SECRET,
+  passwordSalt: process.env.PASSWORD_SALT,
   // CLOUDINARY
   cloudinaryCloudName: process.env.CLOUD_NAME,
   cloudinaryApiKey: process.env.API_KEY,
   cloudinaryApiSecret: process.env.API_SECRET,
-  // MISC
-  jwtSecret: process.env.JWT_SECRET,
-  passwordSalt: process.env.PASSWORD_SALT,
 };
