ea9b63012f07c553777d3c61aec192f114ab628e
Oleh Poliakov <106983325+OlehPoliakov@users.noreply.github.com>
2023-01-20 00:26:25 +0200

    upgrate customer login

--- netlify/functions/customer-login.ts
+++ netlify/functions/customer-login.ts
@@ -18,6 +18,7 @@ const handler: Handler = async (event: HandlerEvent, context: HandlerContext) =>
   }
 
   const input: CustomerLoginInput = JSON.parse(body).input.input;
+
   let phoneNumber;
 
   try {
@@ -26,17 +27,18 @@ const handler: Handler = async (event: HandlerEvent, context: HandlerContext) =>
     return JSON.parse(error.message);
   }
 
+
   const verification = await twilioClient.verify.v2
     .services(config.twilioServiceSid)
     .verifications.create({ to: phoneNumber, channel: 'sms' });
-
-  await api.RegisterNewCustomer(
-    {
-      phone: phoneNumber,
-      twilioVerificationSid: verification.sid,
-    },
-    { 'x-hasura-admin-secret': config.hasuraAdminSecret }
-  );
+    
+    await api.RegisterNewCustomer(
+      {
+        phone: phoneNumber,
+        twilioVerificationSid: verification.sid,
+      },
+      { 'x-hasura-admin-secret': config.hasuraAdminSecret }
+      );
 
   return {
     statusCode: 200,
@@ -45,5 +47,6 @@ const handler: Handler = async (event: HandlerEvent, context: HandlerContext) =>
     }),
   };
 };
+  
 
 export { handler };
